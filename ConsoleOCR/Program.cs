f7daddbbd1e02babba4dd76a35309c78e299fca6
David Abel Islas Hernandez <david.Islas@itconsultingrr.com>
2021-02-04 14:57:25 -0600

    Primera prueba de reconocimiento de carácteres en imagenes

--- ConsoleOCR/Program.cs
+++ ConsoleOCR/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tesseract;
 
 namespace ConsoleOCR
 {
@@ -10,6 +11,30 @@ namespace ConsoleOCR
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\UDI005\source\Repos\PruebaOCR\PruebaOCR\getEdocuentaBajas.PNG";
+            string text = "";
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using(var imag = Pix.LoadFromFile(path))
+                    {
+                        using(var page = engine.Process(imag))
+                        {
+                            text = page.GetText();
+                        }
+                    }
+
+                }                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(text);
+            Console.ReadLine();
         }
     }
 }
