b763c2652f3ac528cf6b7bf9f55cfa0f455771bc
David Abel Islas Hernandez <david.Islas@itconsultingrr.com>
2021-02-15 11:27:51 -0600

    Código para obtener el código postal

--- ConsoleOCR/Program.cs
+++ ConsoleOCR/Program.cs
@@ -21,44 +21,51 @@ namespace GoogleCloudSamples
             string yaz = "yazzine.JFIF";
             string david = "davidine.jpg";
             string brayan = "brayanine.JPG";
-            var image = Image.FromFile(@"davidine.jpg");
+            string recibo = "reciboVic.JPG";
+           
             string clave = "";
             string curp = "";
             
             string resultado = "";
             string resultado2 = "";
-           
+            string resultado3 = "";
 
+            string cp = "";
             // Instantiates a client
             var client = ImageAnnotatorClient.Create();
             // Load the image file into memory
-
+            var image = Image.FromFile(recibo);
             // Performs label detection on the image file
             var response = client.DetectText(image);
             foreach (var annotation in response)
             {
                 if (annotation.Description != null)
-                  //  Console.WriteLine(annotation.Description);
+                 //  Console.WriteLine(annotation.Description);
+                //Aqui se obtiene la clave de elector de la credencial        
+                clave = annotation.Description;
+
+                cp = annotation.Description;
+                var startIndexCP = cp.IndexOf("C.P. ");
+                resultado3 = startIndexCP < 0 ? "" : cp.Substring(startIndexCP, 10);
+
+                //Aqui se obtiene la clave de elector de la credencial        
+                //    clave = annotation.Description;
+
+                //    resultado = clave.Substring(
+                //    clave.IndexOf("CLAVE DE ELECTOR "),
+                //    35);
+
+                ////Aqui se obtiene la curp de la credencial        
+                //    curp = annotation.Description;
+
+                //    resultado2 = curp.Substring(
+                //    curp.IndexOf("CURP "),
+                //    24);
 
 
-                    //Aqui se obtiene la clave de elector de la credencial        
-                    clave = annotation.Description;
 
-                    resultado = clave.Substring(
-                    clave.IndexOf("CLAVE DE ELECTOR "),
-                    35);
-
-                //Aqui se obtiene la curp de la credencial        
-                    curp = annotation.Description;
-
-                    resultado2 = curp.Substring(
-                    curp.IndexOf("CURP "),
-                    24);
-
-
-
-                Console.WriteLine(resultado);
-                Console.WriteLine(resultado2);
+                Console.WriteLine(resultado3);
+                //Console.WriteLine(resultado2);
 
 
                 Console.ReadKey();
